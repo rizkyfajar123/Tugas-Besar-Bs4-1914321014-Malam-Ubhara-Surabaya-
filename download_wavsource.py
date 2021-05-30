6871f5f18e9e95e401e8377236807f29daca14bc
rizkyfajar123 <85070701+rizkyfajar123@users.noreply.github.com>
2021-05-30 22:31:34 +0700

    Add files via upload

---
+++ download_wavsource.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.wavsource.com/snds_2020-10-01_3728627494378403/people/men/about_time.wav'])
